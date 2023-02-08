fbe95d6dae538b2bd87ffec9aea637a2513a2550
Kate <115913025+bootonk@users.noreply.github.com>
2023-02-07 18:40:56 -0800

    Update return value logic to better suit helper function requirements

--- eqArrays.js
+++ eqArrays.js
@@ -6,14 +6,13 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(actualArr, expectedArr) {
-  let result = "";
   if (actualArr.length !== expectedArr.length) {
-    result = false;
+   return false;
   }
 
   for (let i = 0; i < actualArr.length; i++) {
     if (actualArr[i] !== expectedArr[i]) {
-      result = false;
+      return false;
     } else {
       result = true;
     }
@@ -22,9 +21,9 @@ const eqArrays = function(actualArr, expectedArr) {
 };
 
 // Test code
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
