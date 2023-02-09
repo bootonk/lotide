db5e37fd15e70bdbd14c21e81e005f13f0da725b
Kate <115913025+bootonk@users.noreply.github.com>
2023-02-08 18:21:20 -0800

    Corrected spacing issue to ensure correct index locations

--- letterPositions.js
+++ letterPositions.js
@@ -21,15 +21,19 @@ const assertArrayEqual = function(actualArr, expectedArr) {
 
 //letterPosition code (everything above is testing code)
 const letterPositions = function(sentence) {
-  const reducedSentence = sentence.replaceAll(" ", "").toLowerCase();
+  const reducedSentence = sentence.toLowerCase();
   const results = {};
 
   for (let char = 0; char < reducedSentence.length; char++) {
     console.log('letter: ', reducedSentence[char], 'index: ', char);
-    if (!results[reducedSentence[char]]) {
-      results[reducedSentence[char]] = [char];
+    if (reducedSentence[char] === " ") {
+      continue;
     } else {
-      results[reducedSentence[char]].push(char);
+      if (!results[reducedSentence[char]]) {
+        results[reducedSentence[char]] = [char];
+      } else {
+        results[reducedSentence[char]].push(char);
+      }
     }
   }
 
@@ -37,8 +41,19 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-const result1 = letterPositions("hello");
-assertArrayEqual(result1["h"], [0]);
-assertArrayEqual(result1["e"], [1]);
-assertArrayEqual(result1["l"], [2, 3]);
-assertArrayEqual(result1["o"], [4]);
+// const result1 = letterPositions("hello");
+// assertArrayEqual(result1["h"], [0]);
+// assertArrayEqual(result1["e"], [1]);
+// assertArrayEqual(result1["l"], [2, 3]);
+// assertArrayEqual(result1["o"], [4]);
+const result1 = letterPositions("lighthouse in the house");
+assertArrayEqual(result1["l"], [0]);
+assertArrayEqual(result1["i"], [1, 11]);
+assertArrayEqual(result1["g"], [2]);
+assertArrayEqual(result1["h"], [3, 5, 15, 18]);
+assertArrayEqual(result1["t"], [4, 14]);
+assertArrayEqual(result1["o"], [6, 19]);
+assertArrayEqual(result1["u"], [7, 20]);
+assertArrayEqual(result1["s"], [8, 21]);
+assertArrayEqual(result1["e"], [9, 16, 22]);
+assertArrayEqual(result1["n"], [12]);
