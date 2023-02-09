c8935aeccfa297537258baeb44d21fc9bd2bb594
Kate <115913025+bootonk@users.noreply.github.com>
2023-02-08 16:54:49 -0800

    Add function to tally elements in a string based on frequency

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(str) {
+  let reducedStr = str.replaceAll(" ", "").toLowerCase();
+  let letterCount = {};
+  for (let char of reducedStr) {
+    if (letterCount[char]) {
+      letterCount[char]++;
+    } else {
+      letterCount[char] = 1;
+    }
+  }
+  return letterCount;
+};
+
+const result1 = countLetters("Lighthouse in the House");
+console.log(result1);
+
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["g"], 1);
+assertEqual(result1["h"], 4);
+assertEqual(result1["t"], 2);
+assertEqual(result1["o"], 2);
+assertEqual(result1["u"], 2);
+assertEqual(result1["s"], 2);
+assertEqual(result1["e"], 3);
+assertEqual(result1["z"], undefined);
