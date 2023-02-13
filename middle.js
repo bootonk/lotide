71e4e1645f7a435f83db0b1b389bf3dd99e326cc
Kate <115913025+bootonk@users.noreply.github.com>
2023-02-13 09:35:49 -0800

    Update middle function to remove testing to separate files and import/export as needed

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-///////// Testing function
-const eqArrays = function(actualArr, expectedArr) {
-  if (actualArr.length !== expectedArr.length) {
-    return false;
-  }
-
-  for (let i = 0; i < actualArr.length; i++) {
-    if (actualArr[i] !== expectedArr[i]) {
-      return false;
-    } 
-  }
-  return true;
-};
-
-const assertArrayEqual = function(actualArr, expectedArr) {
-  let equalResult = eqArrays(actualArr, expectedArr);
-  if (equalResult) {
-    return console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
-  }
-  return console.log(`🛑🛑🛑 Assertion Failed: ${actualArr} !== ${expectedArr}`);
-};
-
 ///////// Function code
 const middle = function(arr) {
   let midIndex = Math.ceil((arr.length / 2) - 1);
@@ -31,12 +9,5 @@ const middle = function(arr) {
   return midArray;
 };
 
-///////// Testing calls
-assertArrayEqual(middle([]), []);
-assertArrayEqual(middle([1]), []);
-assertArrayEqual(middle([1, 2]), []);
-assertArrayEqual(middle([1, 2, 3]), [2]);
-assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArrayEqual(middle(['hello', 'lighthouse', 'labs']), ['lighthouse']);
+///////// Export function file
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,13 @@
+///////// Testing function file import
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+///////// Testing calls
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle(['hello', 'lighthouse', 'labs']), ['lighthouse']);
